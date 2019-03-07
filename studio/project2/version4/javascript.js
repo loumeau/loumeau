204f5a7d5b97a8d2fad878f41b8bf9d5c7b7f9f3
Kevin Loumeau <loumk819@newschool.org>
2019-03-07 09:41:07 -0500

    kevin

---
+++ studio/project2/version4/javascript.js
@@ -0,0 +1,15 @@
+document.addEventListener(
+  "scroll",
+  function() {
+    var scrollTop =
+      document.documentElement["scrollTop"] || document.body["scrollTop"];
+    var scrollBottom =
+      (document.documentElement["scrollHeight"] ||
+        document.body["scrollHeight"]) - document.documentElement.clientHeight;
+    scrollPercent = scrollTop / scrollBottom * 100 + "%";
+    document
+      .getElementById("_progress")
+      .style.setProperty("--scroll", scrollPercent);
+  },
+  { passive: true }
+);
