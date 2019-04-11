22860e0e0dc37f43e58ffae98edbae018a478640
Kevin Loumeau <loumk819@newschool.org>
2019-04-10 21:18:17 -0400

    kevin

---
+++ lab/exercise7/js.js
@@ -0,0 +1,26 @@
+function showInsta() {
+  var x = document.getElementById("logo2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showSnap() {
+  var x = document.getElementById("logo");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showTwitter() {
+  var x = document.getElementById("logo3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
